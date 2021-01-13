bb66bdc4a99d8b920a31f9e20cade63d7667fde6
Mauricio Sanchez <msanchezm1409@alumno.ipn.mx>
2021-01-13 02:15:24 -0600

    Prueba

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+posts = []
+@app.route("/")
+def index():
+    return "{} posts".format(len(posts))
