f31fe64f85db372bead3420998f2b94de7ec437c
Mauricio Sanchez <msanchezm1409@alumno.ipn.mx>
2021-01-13 02:27:06 -0600

    Agregada la función de Cifrado/Decifrado

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from Crypto.Cipher import AES
+
+
 app = Flask(__name__)
 posts = []
 @app.route("/")
 def index():
     return "{} posts".format(len(posts))
+
+@app.route("/")
+def encrypt(key, data):
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return cipher.nonce + tag + ciphertext
