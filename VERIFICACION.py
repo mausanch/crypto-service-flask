6ada05a8329650f9db746f5915d83bfd0fc0a50d
ChemaLx29 <72090154+ChemaLx29@users.noreply.github.com>
2021-01-25 00:43:44 -0600

    Update VERIFICACION.py
    ya esta lista la funcion de verificar la firma digital del archivo

--- VERIFICACION.py
+++ VERIFICACION.py
@@ -1,28 +1,38 @@
-import errno
 from os import system
-
-from Cryptodome.Hash import SHA256
-from Cryptodome.PublicKey import RSA
-from Cryptodome.Signature import PKCS1_v1_5
-import codecs
-
+import base64
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
 
 
-def verificar(dire,key,message):
-    Verificador=open(dire,'rb').read()
+#funcion que Verifica cualquier documento
+#se deben ingresar las rutas de: el archivo de la llave publica, del archivo a verificar
+#y de la firma digital ** En todas se debe especificar el tipo de archivo **
+def verificacion(firma_digital_in, archivo_a_verificar, llave_publica):
 
-    rec=codecs.getdecoder('hex')
-    Verificador=rec(Verificador)[0]
+    Verificador=open(firma_digital_in,'rb').read()
 
-    message.encode('utf8')
-    message=bytes(message,'utf8')
+    message = open(archivo_a_verificar, 'rb')  ## Leemos ARCHIVO 
+    archivo_leido = message.read()
+    archivo_codificado = base64.b64encode(archivo_leido) #CONVERSION A 64 BITS 
 
-    with open(key, 'rb') as f:
+
+    with open(llave_publica, 'rb') as f:
         key = RSA.importKey(f.read())
-    hasher = SHA256.new(message)
+    hasher = SHA256.new(archivo_codificado)
     verifier = PKCS1_v1_5.new(key)
     if verifier.verify(hasher, Verificador):
-        return print('Verificaccion valida')
+        print('Verificaccion CORRECTA')
     else:
-        return print('Verificación invalida')
+        print('EL MENSAJE NO ES EL MISMO')
+    
+    return 0
 
+
+'''
+firma_digital_in1='firmadoporchema.txt'
+archivo_a_verificar1='Imagen.png'
+llave_publica1='chemapub.PEM'
+
+verificacion(firma_digital_in1,archivo_a_verificar1,llave_publica1)
+'''
