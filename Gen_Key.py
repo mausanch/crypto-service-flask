c84c607580dc72f1def400c9a44f7a34138be12f
ChemaLx29 <72090154+ChemaLx29@users.noreply.github.com>
2021-01-25 00:54:05 -0600

    Update Gen_Key.py

--- Gen_Key.py
+++ Gen_Key.py
@@ -10,16 +10,14 @@ def generarFirma(clave_privada_out, clave_publica_out):
 
         with open(clave_privada_out, 'wb') as f:
                 f.write(key.exportKey('PEM'))
+                f.close()
         with open(clave_publica_out, 'wb') as f:
                 f.write(key.publickey().exportKey('PEM'))
+                f.close()
         return 0
 
-
-
-'''
+''' #ejemplo
 clave_privada_out1='chemapriv.PEM'
 clave_publica_out1='chemapub.PEM'
-
 generarFirma(clave_privada_out1,clave_publica_out1)
-
 '''
